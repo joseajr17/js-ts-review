3f3f941c91a32ab30cbcc3f9dbafe959fae11024
jose <josejr017@gmail.com>
2025-07-10 11:08:37 -0300

    enum type and unknowm type"

---
+++ ts/src/enum-type/index.ts
@@ -0,0 +1,10 @@
+enum Colors {
+    RED,
+    BLUE,
+    GREEN
+}
+
+console.log(Colors);
+console.log(Colors.RED);
+console.log(Colors[0]);
+
---
+++ ts/src/unknowm-type/index.ts
@@ -0,0 +1,11 @@
+let x: unknown;
+
+x = 10;
+x = 'Antonio';
+x = 9;
+x = 'Jr';
+x = 800;
+const y = 2;
+
+if (typeof x === 'number')
+    console.log(x * y);
